b96b9c49821b21dbf67a009f0e05daf72ac0b308
Mainao Boro <mainao1230@gmail.com>
2025-10-21 01:02:54 +0530

    chore: ignore ESLint during Next.js build

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "api.trace.moe",
-            },
-            {
-                protocol: "https",
-                hostname: "s4.anilist.co",
-            },
-        ],
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.trace.moe",
+      },
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
