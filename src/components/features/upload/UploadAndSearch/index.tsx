d85ccb7444a613322d544f2b976f09a3fa0f816a
Mainao Boro <mainao1230@gmail.com>
2025-11-02 23:59:57 +0530

    feat: add upload preview

--- src/components/features/upload/UploadAndSearch/index.tsx
+++ src/components/features/upload/UploadAndSearch/index.tsx
@@ -5,6 +5,7 @@ import UploadBox from "../UploadBox";
 import LoadingSpinner from "@/components/ui/Spinner";
 import AnimeResultGrid from "@/components/features/results/AnimeResultGrid";
 import { useAnimeSearch } from "@/hooks/useAnimeSearch";
+import Image from "next/image";
 
 export default function UploadAndSearch() {
     const [highlight, setHighlight] = useState(false);
@@ -59,6 +60,25 @@ export default function UploadAndSearch() {
 
             {animeSearch.isPending && (
                 <LoadingSpinner className="w-full mt-6" size={400} />
+            )}
+
+            {animeSearch.isSuccess && (
+                <div className="md:w-1/3 space-y-4 mt-6">
+                    <div className="bg-neutral-900 rounded-2xl p-4">
+                        <h2 className="text-gray-400 text-sm mb-2">Your search image</h2>
+                        <div className="relative w-full h-48 md:h-64">
+                            <Image
+                            src={previewImageUrl!}
+                            alt="Search"
+                            fill
+                            className="object-cover rounded-xl border border-neutral-700"
+                            />
+                        </div>
+                        <p className="text-xs text-gray-500 mt-2">
+                            2560×1440 • ~36.56% similarity
+                        </p>
+                    </div>
+                </div>
             )}
 
             {animeSearch.isSuccess && (
---
+++ src/components/features/upload/UploadPreview.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+
+export default function UploadPreview({previewImageUrl}) {
+    return (
+        <>
+            <div className="md:w-1/3 space-y-4 mt-6">
+                <div className="bg-neutral-900 rounded-2xl p-4">
+                    <h2 className="text-gray-400 text-sm mb-2">Your search image</h2>
+                    <div className="relative w-full h-48 md:h-64">
+                        <Image
+                        src={previewImageUrl!}
+                        alt="Search"
+                        fill
+                        className="object-cover rounded-xl border border-neutral-700"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
